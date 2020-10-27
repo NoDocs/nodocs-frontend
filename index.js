6951825c1589ea21ab85e9da44349870288366fc
ggunia <guramgunia@gmail.com>
2020-10-27 21:33:55 +0400

    fix: rolled back

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import App from './src/App'
+
 const Index = () => {
-  return (
-    <div>mdaa</div>
-  )
+  return <App />
 }
 
 ReactDOM.render(<Index />, document.getElementById('root'))
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>Starting Point !!</div>
+  )
+}
+
+export default App
