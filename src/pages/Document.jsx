e4966ae80e4dac5ba290d11eece3b7d02599518f
ggunia <guramgunia@gmail.com>
2020-10-31 13:38:26 +0400

    feat: initial setup for document

--- src/pages/Document.jsx
+++ src/pages/Document.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Slate, Editable, withReact } from 'slate-react'
+import { createEditor } from 'slate'
+import styled from 'styled-components'
+
+const StyledEditorContainer = styled.div`
+  margin: 50px;
+  border: 1px solid gray;
+  padding: 20px;
+`
 
 const Document = () => {
+  const [editorState, updateEditorState] = React.useState([
+    { type: 'paragraph', children: [{ text: '' }] }
+  ])
+  const editor = React.useMemo(
+    () => withReact(createEditor()),
+    []
+  )
+
   return (
-    <div>Document</div>
+    <StyledEditorContainer>
+      <Slate
+        editor={editor}
+        value={editorState}
+        onChange={updateEditorState}
+      >
+        <Editable />
+      </Slate>
+    </StyledEditorContainer>
   )
 }
 
