2d463c8caf461935a85a6c9358e1207cf9ca1a23
ggunia <guramgunia@gmail.com>
2020-11-01 23:24:17 +0400

    feat: basic auth redux

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
 
+import * as authServices from 'services/auth'
 import history from 'utils/history'
 
 const Login = () => {
+  const [submitting, toggleSubmitting] = React.useState()
+
+  const handleSignIn = (event) => {
+    event.preventDefault()
+    toggleSubmitting(true)
+
+    const { email, password } = document.authForm.elements
+
+    const data = {
+      email: email.value,
+      password: password.value,
+    }
+
+    authServices.signIn(data)
+      .then(response => {
+        const { data: { token } } = response
+
+        localStorage.setItem('token', token)
+        history.push('/')
+      })
+  }
+
   return (
-    <React.Fragment>
+    <form name="authForm" onSubmit={handleSignIn}>
       <input placeholder="email" />
-      <input placeholder="password" />
+      <input type="password" placeholder="password" />
       <button>Login</button>
 
       <p onClick={() => history.push('/register')}>Register</p>
-    </React.Fragment>
+    </form>
   )
 }
 
---
+++ src/redux/auth/index.js
@@ -0,0 +1,3 @@
+export { default as authActionTypes } from './authActionTypes'
+export { default as authActions } from './authActions'
+export { default as authReducer } from './authReducer'
