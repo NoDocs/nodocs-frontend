52eff1c2e8a67aa548ffff2aeaa7837073fb357b
ggunia <guramgunia@gmail.com>
2020-11-14 00:17:17 +0400

    feat: added logo

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import logoIcon from 'assets/logo.svg'
+
 const StyledContainer = styled.div`
   height: 56px;
   display: flex;
   align-items: center;
   background-color: black;
   grid-area: nav;
+  padding-left: 30px;
 `
 
 const Navigation = () => (
   <StyledContainer>
-
+    <img src={logoIcon} alt="NoDocs" />
   </StyledContainer>
 )
 
