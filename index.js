35bda3aaac67795b3ef61ad3480ed56d4a94407c
saba-coding <saba.chikhina@gmail.com>
2020-12-03 20:09:22 +0400

    chore: test push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { Router } from 'react-router-dom'
 
 import history from './src/utils/history'
 import App from './src/App'
