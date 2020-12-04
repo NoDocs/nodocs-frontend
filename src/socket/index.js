10735d793081820f5ba83c387fe158e02aeb71a7
saba-coding <saba.chikhina@gmail.com>
2020-12-04 21:29:38 +0400

    chore: init

--- src/socket/index.js
+++ src/socket/index.js
@@ -13,14 +13,31 @@ class Socket {
     return new Promise((resolve, reject) => {
       if (Socket.socket) return
 
-      const socket = io(process.env.BASE_API_URL, {
+      const token = localStorage.getItem('token')
+      const socket = io(`${process.env.BASE_API_URL}/company-f04bc600-c835-4022-9833-75a27cb74497`, {
         reconnectionDelayMax: 10000,
         transports: ['websocket'],
-        secure: true
+        secure: true,
+        query: {
+          token: token.startsWith('Bearer') ? token.slice(7) : token
+        }
       })
 
       socket.on('connect', () => {
         Socket.socket = socket
+        console.log("🚀 ~ file: index.js ~ line 31 ~ Socket ~ socket.on ~ socket", socket)
+        socket.on('document-created', (ffff) => {
+          console.log("🚀 ~ file: index.js ~ line 30 ~ Socket ~ socket.on ~ ffff", ffff)
+        })
+        socket.on('document-updated', (ffff) => {
+          console.log("🚀 ~ file: index.js ~ line 30 ~ Socket ~ socket.on ~ ffff", ffff)
+        })
+        socket.on('team-created', (ffff) => {
+          console.log("🚀 ~ file: index.js ~ line 30 ~ Socket ~ socket.on ~ ffff", ffff)
+        })
+        socket.on('document-created', (ffff) => {
+          console.log("🚀 ~ file: index.js ~ line 30 ~ Socket ~ socket.on ~ ffff", ffff)
+        })
         resolve()
       })
 
