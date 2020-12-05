eaa2940544d7503e59060dab753959a43e912c45
gtsitsvidze <gtsitsvidze97@gmail.com>
2020-12-05 15:52:55 +0400

    feat: documents by collection

--- src/pages/Team/Collection.jsx
+++ src/pages/Team/Collection.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import * as teamServices from 'services/team'
 import { documentActions } from 'logic/document'
 
+import Table from 'molecules/Table'
+import Toggle from 'molecules/Toggle'
+import UserIcon from 'assets/userIcon.svg'
+
 const Collection = ({ id }) => {
   const dispatch = useDispatch()
-
+  const documents = useSelector(state => state.getIn(['entities', 'documents']))
   React.useEffect(
     () => {
       teamServices
@@ -22,7 +26,22 @@ const Collection = ({ id }) => {
   )
 
   return (
-    <div>Collection: {id}</div>
+    <>
+      <Toggle title={`Collection: ${id}`}>
+        <Table
+          proportions="38px 405px 1fr 1fr 1fr 1fr"
+          headerTabs={[
+            { content: '', position: '' },
+            { content: 'Name', position: 'left' },
+            { content: 'Subscribers', position: 'center' },
+            { content: 'Linked to', position: 'center' },
+            { content: 'Mentions', position: 'center' },
+            { content: <img key="icon" src={UserIcon} />, }
+          ]}
+          data={documents}
+        />
+      </Toggle>
+    </>
   )
 }
 
