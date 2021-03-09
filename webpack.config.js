4b67479c7e4d75f3e4528b6a26c8172e04634e74
ggunia <guramgunia@gmail.com>
2021-03-09 14:32:18 +0400

    fix: basic relay configuration

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
             plugins: [
+              'relay',
               '@babel/plugin-transform-runtime',
               '@babel/plugin-proposal-class-properties',
               '@babel/plugin-syntax-dynamic-import',
